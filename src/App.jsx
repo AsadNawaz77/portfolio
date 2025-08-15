d7079cdcc3eb4311a21877a9fedf7044d6e6cb92
AsadNawaz77 <132277348+AsadNawaz77@users.noreply.github.com>
2025-08-16 01:11:04 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import {
 
 import { SiTailwindcss } from "react-icons/si";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
 function App() {
   const [activeCardIndex, setActiveCardIndex] = useState(0);
   const cards = [
@@ -214,6 +215,19 @@ function App() {
       <Testimonials />
       <Contact />
       <Footer />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </>
   );
 }
